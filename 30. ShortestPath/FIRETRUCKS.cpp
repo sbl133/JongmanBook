7c9049cb3820eaef4002d800f41471201a336aea
sbl133 <70326426+sbl133@users.noreply.github.com>
2022-03-08 00:23:20 +0900

    Update FIRETRUCKS.cpp

--- 30. ShortestPath/FIRETRUCKS.cpp
+++ 30. ShortestPath/FIRETRUCKS.cpp
@@ -3,7 +3,7 @@
 #include<vector>
 using namespace std;
 // 그래프의 인접리스트. pair(연결된 정점, 간선 가중치)
-vector<pair<int, int>> adj[1000];
+vector<pair<int, int>> adj[1001];
 int fired[1000];
 int firehouse[1000];
 vector<int> dijkstra(int v) {
@@ -15,10 +15,13 @@ vector<int> dijkstra(int v) {
 		int here = pq.top().second;
 		int cost = -pq.top().first;
 		pq.pop();
+		// 만약 지금 꺼낸것보다 더 짧은 경로를 이미 확인했으면 그만둔다
 		if (dist[here] < cost)continue;
+		// 인접 정점들을 검사
 		for (int i = 0; i < adj[here].size(); i++) {
 			int there = adj[here][i].first;
 			int nextDist = adj[here][i].second + cost;
+			// 더 짧은 경로 발견시 dist갱신 후 우선순위 큐에 삽입
 			if (nextDist < dist[there]) {
 				dist[there] = nextDist;
 				pq.push(make_pair(-nextDist, there));
@@ -32,7 +35,7 @@ int main() {
 	cin >> testcase;
 	while (testcase--) {
 		int v, e, n, m;
-		for (int i = 0; i < 1000; i++)
+		for (int i = 0; i < 1001; i++)
 			adj[i].clear();
 		cin >> v >> e >> n >> m;
 		for (int i = 0; i < e; i++) {
