c42e1c716c2c547d2ba18e6b7633a36b3e91453f
sbl133 <70326426+sbl133@users.noreply.github.com>
2022-03-12 10:51:49 +0900

    Update NTHLON.cpp

--- 30. ShortestPath/NTHLON.cpp
+++ 30. ShortestPath/NTHLON.cpp
@@ -1,78 +1,3 @@
-/*#include<queue>
-#include<vector>
-#include<iostream>
-using namespace std;
-
-int V;
-const int INF = 987654321;
-vector<pair<int, int>> adj[410];
-const int START = 401;
-vector<int> dijkstra(int src) {
-	vector<int> dist(V, INF);
-	dist[src] = 0;
-	priority_queue<pair<int, int>> pq;
-	pq.push(make_pair(0, src));
-	while (!pq.empty()) {
-		int here = pq.top().second;
-		int cost = -pq.top().first;
-		pq.pop();
-		if (cost > dist[here])continue;
-		for (int i = 0; i < adj[here].size(); i++) {
-			int there = adj[here][i].first;
-			int nextDist = cost + adj[here][i].second;
-			if (dist[there] > nextDist) {
-				dist[there] = nextDist;
-				pq.push(make_pair(-nextDist, there));
-			}
-		}
-	}
-	return dist;
-}
-
-int vertex(int delta) {
-	return 200 + delta;
-}
-int solve(const vector<int>& a, const vector<int>& b) {
-	V = 402;
-	for (int i = 0; i < V; i++)
-		adj[i].clear();
-	for (int i = 0; i < a.size(); i++) {
-		int delta = a[i] - b[i];
-		adj[START].push_back(make_pair(vertex(delta), a[i]));
-	}
-	for (int delta = -200; delta <= 200; delta++) {
-		for (int i = 0; i < a.size(); i++) {
-			int next = delta + a[i] - b[i];
-			if (abs(next) > 200)continue;
-			adj[vertex(delta)].push_back(make_pair(vertex(next), a[i]));
-		}
-	}
-	vector<int> shortest = dijkstra(START);
-	int ret = shortest[vertex(0)];
-	if (ret == INF) return -1;
-	return ret;
-}
-int main() {
-	int testcase;
-	cin >> testcase;
-	while (testcase--) {
-		int games;
-		cin >> games;
-		vector<int> a, b;
-		int time1, time2;
-		int result;
-		while (games--) {
-			cin >> time1 >> time2;
-			a.push_back(time1);
-			b.push_back(time2);
-		}
-		result = solve(a, b);
-		if (result == -1)
-			cout << "IMPOSSIBLE" << endl;
-		else
-			cout << result << endl;
-	}
-}*/
 #include<iostream>
 #include<vector>
 #include<queue>
@@ -92,8 +17,8 @@ vector<int> dijkstra(int src) {
 		pq.pop();
 		if (cost > dist[here])continue;
 		for (int i = 0; i < adj[here].size(); i++) {
-			int there = adj[here][i].second;
-			int nextDist = cost + adj[here][i].first;
+			int there = adj[here][i].first;
+			int nextDist = cost + adj[here][i].second;
 			if (nextDist < dist[there]) {
 				dist[there] = nextDist;
 				pq.push(make_pair(-nextDist, there));
@@ -145,6 +70,10 @@ int main() {
 			a.push_back(timeA);
 			b.push_back(timeB);
 		}
-		cout << solve(a, b) << endl;
+		int result = solve(a, b);
+		if (result == -1)
+			cout << "IMPOSSIBLE" << endl;
+		else
+			cout << result << endl;
 	}
 }
